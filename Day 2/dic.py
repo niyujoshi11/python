d24a6f369d1d288dd9967cdd9edf430e1b1fdf15
Nihir Zala <zalanihir30@gmail.com>
2023-09-26 22:12:57 +0530

    added dictionary  & update the set

---
+++ Day 2/dic.py
@@ -0,0 +1,31 @@
+# In Python, dictionaries are written with curly brackets { } , 
+# and store key-value pairs. Dictionary keys should be the immutable Python object, 
+# for example, number, string, or tuple. Keys are case sensitive. Duplicate keys are not allowed in the dictionary
+
+data = {0:0, 1:"Nihir", 2:"Zala", 3:"Test"}
+
+# data uses index key
+data[1]
+
+data.get(4, "Not got it, Create one")
+
+keys = ["Dell", "Nihir", "Tesst"]
+values = ["System", "Zala", "Here"]
+
+newdata = dict(zip(keys,values))
+
+data["Dell"]
+
+# for added the new list
+data["newadd"] = "val-here"
+
+del data["newadd"]
+
+# Dic in dic
+
+pro = {'js':'atom', 'cs':'vs', 'python':['pycharm','sublime'], 'java':{'jse':'netbeans','jee':'eclips'}}
+
+print(pro)
+print(pro["cs"])
+print(pro["python"][1])
+print(pro["python"])
--- Day 2/set.py
+++ Day 2/set.py
@@ -2,4 +2,5 @@
 # set uses curly brackets
 # in set indexing is not supported
 # set output is unique
+
 s = {11, 22, 33, 44, 55, 66, 77, 88, 99, 00, 11}
