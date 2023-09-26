7ff0da028173485da808ddad1aa8aa5d45e408a0
Nihir Zala <zalanihir30@gmail.com>
2023-09-26 21:34:17 +0530

    update the string file

--- Day 1/string.py
+++ Day 1/string.py
@@ -1,11 +1,19 @@
-
+# strings are immutable in python
 # String Demo
 print("nihir's " "Laptop")
 
-a = "Hello World"
+a = "youtube"
 print(a)
 
-b = a[:3] # output will be "Hel"
-b = a[3:4] # output will be "l"
-b = a[-1:] # output will be "d"
+b = a[:3]  # output will be "yout"
+b = a[3:4] # output will be "tu"
+b = a[-1:] # output will be "e"
 print(b)
+
+
+newname = "My" + a[3:]
+print(newname)
+
+# for getting the length of string len
+
+print(len(newname))
