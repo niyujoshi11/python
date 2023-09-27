ce626b32e7fe2e39040bab133c08d2f84ba316d2
Nihir Zala <zalanihir30@gmail.com>
2023-09-27 12:27:40 +0530

    added the operators files

---
+++ Day 2/operators.py
@@ -0,0 +1,44 @@
+# arithmetic Operators
+
+x = 2
+y = 3
+
+x + y
+x - y
+x * y
+x / y
+
+# assignment operators
+# used symbol is "equal (=)"
+
+x = x + 2
+
+x + = 2
+
+# Both are same
+
+a, b = 5, 6
+
+# unary operators
+
+n = 7
+
+-n # got nagative value
+
+n = -n # assign nagative value
+
+# realational operators  returns true or false
+
+a < b # for check it returns true or false
+
+a == b # for compare
+
+a > b 
+a <= b  # less than or equal to
+a != b  # not equal to
+
+# logical operators AND, OR, NOT
+
+a < 8 and b < 8
+a > 8 and b < 8
+a > 8 and b > 8
