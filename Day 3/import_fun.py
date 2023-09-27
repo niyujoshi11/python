5767ad5b55834580a1312d95f3c44ca76d835c54
Nihir Zala <zalanihir30@gmail.com>
2023-09-27 21:14:26 +0530

    added the import function usecase

---
+++ Day 3/import_fun.py
@@ -0,0 +1,21 @@
+# use import for import the functions
+
+import math
+
+# for ex want to get the sqrt of 25
+
+print("sqrt of 25 is :",math.sqrt(25))
+print("sqrt of 15 is :",math.sqrt(15))
+print("floor of 2.9 is :",math.floor(2.9))
+print("ceil of 2.2 is :",math.ceil(2.2))
+
+# here you can use all mathemetical functions
+
+print("The value of pow 3 of 2 is :", math.pow(3,2))
+print("The value of pi is :", math.pi())
+
+# you can use like this
+
+import math as m # after this you just have to call m.funname()
+
+print("Value of pi is:", m.pi)
