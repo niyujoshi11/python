75b9fe54202dff68631d6a8ccd2f317a46cf3653
Nihir Zala <zalanihir30@gmail.com>
2023-09-28 12:59:38 +0530

    update the for loop

---
+++ Day 3/for_loop.py
@@ -0,0 +1,20 @@
+# for loop come with range or list
+x = [1,2,3,4,5,6]
+
+for i in x:
+    print(i)
+
+# for loop used be with sequence
+x = "Nihir"
+
+for i in x:
+    print(i)
+
+for i in range(54):
+    print(i)
+
+# for i in range(11,21,2):
+#     print(i)
+
+# for i in range(20, 10, -1):
+#     print(i)
