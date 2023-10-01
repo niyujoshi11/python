a67ff262db31bf29c720ffd0892621883fb43878
Nihir Zala <zalanihir30@gmail.com>
2023-10-01 11:12:54 +0530

    added the user eval

---
+++ Day 3/user_input.py
@@ -0,0 +1,8 @@
+# By default input gives you a string
+x = input("Enter A number")
+y = input("Enter B number")
+
+# input funcation has eval function for using expression
+
+evalReasult = eval(input("Enter expression")) # ex:2 +3 +5 -2
+print(evalReasult)
