03bb2f510b60097f1f03e634a5726cd87708e9f8
Nihir Zala <zalanihir30@gmail.com>
2023-10-13 17:41:55 +0530

    remove the white space

--- Day 9/1Abstrackt.py
+++ Day 9/1Abstrackt.py
@@ -1,25 +1,19 @@
 # when you pass the pass in method which have nothing to return than your method called abstract method
-
-
 from abc import ABC,abstractclassmethod
-
 class Computer(ABC):
     @abstractclassmethod
     def process(self):
         pass
         # print("Running")
 
-
 class Laptop(Computer):
     # pass
     def process(self):
         print("New running")
 
-
 class Program:
     def work(self):
         print("Hello Programmer")
-
 
 # com =Computer()
 # com.process()
